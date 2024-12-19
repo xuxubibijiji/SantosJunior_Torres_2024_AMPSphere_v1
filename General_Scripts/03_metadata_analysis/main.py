c6e8f1f45c371114f20a603afd553e7da5b8ebf6
xuxubibijiji <127366228+xuxubibijiji@users.noreply.github.com>
2024-12-19 13:00:50 +0800

    Update main.py

--- General_Scripts/03_metadata_analysis/main.py
+++ General_Scripts/03_metadata_analysis/main.py
@@ -1,14 +1,28 @@
 import sys
 import os
+print("Current working directory:", os.getcwd())
+print("Python search paths:", sys.path)
 sys.path.append(os.path.join(os.getcwd(), "utils"))
 
 #from utils.download_files import inputsgen
 from utils.gtdb2pgenomes import conversion_from_GTDB_to_pGenomes
+print("gtdb2pgenomes imported successfully!")
+
 from utils.preprocess import filtermmseqs2_amp_contig
+print("preprocess imported successfully!")
+
 from utils.progenomes_genes import ampsphere2progenomes
+print("progenomes_genes imported successfully!")
+
 from utils.metadata import metadata
+print("metadata imported successfully!")
+
 from utils.addspecI import addspecI
+print("addspecI imported successfully!")
+
 from utils.hr_envo import hrenvo
+print("hr_envo imported successfully!")
+
 
 def main():
 #    print('Download inputs')
